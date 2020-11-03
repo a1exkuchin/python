e12b4452a9fe5db72e960dbe944fbe982820af90
a1exkuchin <alexkuchin@list.ru>
2020-11-03 14:40:59 +0500

    Initial commit

--- 2.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-#Представлен список чисел. Необходимо вывести элементы исходного списка, 
-#значения которых больше предыдущего элемента.
-
-source = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-destination = [source[i] for i in range(1,len(source)) if source[i] > source[i-1]]
-print(' '.join(map(str,destination)))
